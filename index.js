83ec5f0ec33383488a5ffba85bccc74d32508d91
gang zhang <gang.zhang@pps.io>
2018-07-06 09:35:05 -0400

    refactor

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const route = require('./api/route');
 
 const server = restify.createServer();
 route.route(server);
-//for (let i = 0; i < 100;i++)
-//console.log(getd.GetTodaysSearch(moment().add(i,'days')));
-//console.log(getd.loadData());
-//getd.SendEmail();
+
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
