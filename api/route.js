1b158dc2692af2c41cdff6568fa0a46219708eb8
Gang Zhang <gzhangx@hotmail.com>
2020-07-25 22:49:41 -0400

    adding cors

--- api/route.js
+++ api/route.js
@@ -1,5 +1,12 @@
 const keys = require('lodash/keys');
 const sender = require('./controllers/sender');
+
+function addCORS(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", req.header("Access-Control-Request-Method"));
+    res.header("Access-Control-Allow-Headers", req.header("Access-Control-Request-Headers"));
+}
+
 const routes = {
     '/sendToday': {
         method: 'get',
@@ -40,11 +47,25 @@ const routes = {
 };
 
 module.exports = {
-    route: server=>{
+    route: server => {
+        
+        server.use((req, res, next) => {
+            if (req.method !== 'GET' && req.method !== 'POST') return next();
+            addCORS(req, res);
+            const controller = routes[req.url];
+            if (controller && controller.auth !== false) {
+                if (!req.user) {
+                    res.send(401, 'Unauthorized');
+                    return next(false);
+                }
+            }
+            return next();
+        }); 
+        
         const rts = keys(routes);
         rts.forEach(url=>{
             const op = routes[url];
             server[op.method](url, op.func);
-        });
+        });        
     }
 };
