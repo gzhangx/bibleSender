768e9b0caac31b94db8a5a5cde01ac1a74a1a78a
Gang Zhang <gzhangx@hotmail.com>
2020-08-23 10:00:41 -0400

    record on schedule

--- tests/sundayrequest.js
+++ tests/sundayrequest.js
@@ -1,15 +1,29 @@
 const acccn = require('../lib/youtubeacccn');
 
-
+function getHourMin(h, m) {
+    return h * 60 + m;
+}
+const STARTHOUR = getHourMin(9, 45);
+const ENDHOUR = getHourMin(11, 15);
 function scheduledAt5() {
     const coeff = 1000 * 60 * 5;
     const date = new Date();  //or use any other date
-    const rounded = new Date(Math.round(date.getTime() / coeff) * coeff + coeff);
-    const wait = rounded - date.getTime();
-    console.log(`waiting ${wait / 1000}`);
+    const curTime = date.getTime();
+    const curHourMin = getHourMin(date.getHours(), date.getMinutes());
+    const rounded = new Date(Math.round(curTime / coeff) * coeff + coeff);
+    const wait = rounded - curTime;
+    
+    
+    if (curHourMin < STARTHOUR) {
+        console.log(`waiting ${wait / 1000}, not started yet`);
+        return setTimeout(scheduledAt5, wait);    
+    } else if (curHourMin > ENDHOUR) {
+        console.log('all done');
+    }
+    console.log(`waiting ${wait / 1000} and record`);
     setTimeout(doSundayVideoRequestEvery5Min, wait);
 }
-async function doSundayVideoRequestEvery5Min() {    
+async function doSundayVideoRequestEvery5Min() {
     acccn.recordAcccnYoutubeCntAll().then(res => {
         console.log(res);
     }).catch(err => {
@@ -19,5 +33,4 @@ async function doSundayVideoRequestEvery5Min() {
     });
 }
 
-acccn.recordAcccnYoutubeCntAll();
 scheduledAt5();
