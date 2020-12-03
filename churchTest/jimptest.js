1b305cdce6b4bbf9ff61bc096b48f9d2ca06f839
Gang Zhang <gzhangx@hotmail.com>
2020-12-02 21:11:39 -0500

    adding access

--- churchTest/jimptest.js
+++ churchTest/jimptest.js
@@ -1,8 +1,21 @@
 
 const { submitFile } = require('../lib/localMissionFile');
-submitFile({
-    payeeName: 'Lian',
-    reimbursementCat: '1604A',
-    amount: 500,
-    description: 'fake desc',
-});
+function safeHouse() {
+    submitFile({
+        payeeName: 'Lian',
+        reimbursementCat: '1604A',
+        amount: 500,
+        description: 'fake desc',
+    });
+}
+
+function access() {
+    submitFile({
+        payeeName: 'Rebekah Wang',
+        reimbursementCat: 'Organization support',
+        amount: '3500',
+        description: '05 Organization Support(Access Support) 1604 $3,500',
+    });
+}
+
+access();
