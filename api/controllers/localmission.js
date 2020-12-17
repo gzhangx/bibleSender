8fbf1ab41f2740126cbffdc5ff2acd203dbd1e37
Gang Zhang <gzhangx@hotmail.com>
2020-12-17 11:15:00 -0500

    adding doCC

--- api/controllers/localmission.js
+++ api/controllers/localmission.js
@@ -1,11 +1,12 @@
 const get = require('lodash/get');
 const lm = require('../../lib/localMissionFile');
+const creds = require('../../credentials.json')
 function getLmCategories(req, res) {    
     return res.send(lm.getCategories());    
 }
 
 function emailExpense(req, res) {
-    const { amount, payee, categary, attachements } = req.body;
+    const { amount, payee, categary, attachements, doCC } = req.body;
     console.log(`${amount} ${payee} ${categary}`);
     lm.submitFile({
         payeeName: payee,
@@ -13,6 +14,7 @@ function emailExpense(req, res) {
         amount: amount,
         description: `${payee} ${amount} ${categary}`,
         attachements,
+        ccList: doCC ? creds.treasureEmails:[],
     }).then(() => {
         res.send({message: 'done'})
     })
